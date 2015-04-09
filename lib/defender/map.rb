c8b1c2cc81eb7e6ea83a07108f5256d64254d229
Alan Willms <dyulax@gmail.com>
2015-04-09 15:30:59 -0300

    Responsive map size

--- lib/defender/map.rb
+++ lib/defender/map.rb
@@ -4,15 +4,13 @@ module Defender
 
     def initialize(window)
       @window = window
-      @height = 14
-      @width = 16
-      @monster_spawner_point = [0, 0]
-      @tower_point = [@width - 1, @height - 1]
+      # @monster_spawner_point = [0, 0]
+      # @tower_point = [columns - 1, rows - 1]
     end
 
     def draw
-      for column in 0..@width do
-        for row in 0..@height do
+      for column in 0..columns do
+        for row in 0..rows do
           x = column * tile_size
           y = row * tile_size
           z = ZOrder::Background
@@ -30,6 +28,14 @@ module Defender
 
       def get_tile
         Gosu::Image.new(@window, GRASS_TILE, false)
+      end
+
+      def columns
+        @columns ||= @window.width.to_i / tile_size.to_i
+      end
+
+      def rows
+        @rows ||= @window.height.to_i / tile_size.to_i
       end
   end
 end
